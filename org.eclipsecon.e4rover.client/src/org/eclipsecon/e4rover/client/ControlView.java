394b77eb7d1bbe84725aa8ee7b8f2f252ce0de0a
admin <admin@orionhub.org>
2011-05-06 07:27:30 -0400

    commit from orionhub

--- org.eclipsecon.e4rover.client/src/org/eclipsecon/e4rover/client/ControlView.java
+++ org.eclipsecon.e4rover.client/src/org/eclipsecon/e4rover/client/ControlView.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ * 	   John Arthorne - Hello from orionhub.org
  ******************************************************************************/
 package org.eclipsecon.e4rover.client;
 
